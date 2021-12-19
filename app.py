80103f9b45285508b902de2fbd21a9e2030ec683
fang2649 <88994790+fang2649@users.noreply.github.com>
2021-12-19 17:57:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -149,9 +149,13 @@ def event_handle(event):
 
     if msgType == "text":
         msg = str(event["message"]["text"])
-        replyObj = TextSendMessage(text=msg)
+        if (msg == "สวัสดี") :
+        replyObj = TextSendMessage(text=ดีจ้า)
+    else :
+         replyObj = TextSendMessage(text=msg)
+       line_bot_api.reply_message(rtoken, replyObj)
         line_bot_api.reply_message(rtoken, replyObj)
-    elif msgType == "covid":
+    elif msgType == "":
         try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
             i = Image.open(BytesIO(message_content.content))
